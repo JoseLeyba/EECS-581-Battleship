9bdb0351c5023fc256907bdc160667a40f0183b3
Gavin Kirwan <gavin.kirwan@icloud.com>
2024-09-11 17:52:54 -0500

    fireShot

--- board.py
+++ board.py
@@ -131,7 +131,7 @@ class Board:
 
             print("Ship has been sunk!")
         
-        # Return true if shot successfully fired
+        # Return true if shot is valid
         return True
 
     def gameOver(self):  # returns true if every ship has sunk 
