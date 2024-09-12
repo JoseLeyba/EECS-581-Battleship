67a2151365e3a6851feadddf88f94d7ccf5f7b6c
EdgarMC22 <143836543+EdgarMC22@users.noreply.github.com>
2024-09-11 20:11:33 -0500

    Add files via upload

--- ship.py
+++ ship.py
@@ -6,12 +6,33 @@ class Ship:
         self.hits = [False] * length  # a bool for each coordinate representing a hit
 
     def addCoordinates(self, start, end):  # populate self.coordinates based on start and end positions
-        pass
+        letter_list = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
+        self.coordinates.append(start)
+        if self.length == 2:
+            self.coordinates.append(end)
+        else:
+            start_list = list(start)
+            end_list = list(end)
+            if len(end_list) == 3:
+                end_list.pop()
+                end_list[1] = str(end_list[1]) + str(end_list[2]) 
+            if start_list[0] == end_list[0]:
+                for i in range(int(end_list[1]) - int(start_list[1])):
+                    self.coordinates.append(str(start_list[0]) + str(i + int(start_list[1]) + 1))
+            else:
+                i = 0
+                while start_list[0] != letter_list[i]:
+                    i += 1
+                while end_list[0] != letter_list[i + 1]:
+                    self.coordinates.append(str(letter_list[i + 1]) + str(start_list[1]))
+                    i += 1
+                self.coordinates.append(end)
 
     def addHit(self, coordinate):  # called by the board if a ship is hit; should update bool in hits list
-        pass
+        for i in self.coordinates:
+            if i == coordinate:
+                self.hits[self.coordinates.index(i)] = True
 
     def isSunk(self):  # returns true if all coordinates of a ship have been hit 
-        pass
-
+        return all(i == True for i in self.hits)
     
