9e98cd9a3476ff3c40a6e10dc253109727b6cd38
EdgarMC22 <143836543+EdgarMC22@users.noreply.github.com>
2024-09-12 14:32:19 -0500

    Add files via upload

--- ship.py
+++ ship.py
@@ -5,28 +5,8 @@ class Ship:
         self.coordinates = []  # list of coordinates; ex: ['A1', 'A2', 'A3']
         self.hits = [False] * length  # a bool for each coordinate representing a hit
 
-    def addCoordinates(self, start, end):  # populate self.coordinates based on start and end positions
-        letter_list = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
-        self.coordinates.append(start)
-        if self.length == 2:
-            self.coordinates.append(end)
-        else:
-            start_list = list(start)
-            end_list = list(end)
-            if len(end_list) == 3:
-                end_list.pop()
-                end_list[1] = str(end_list[1]) + str(end_list[2]) 
-            if start_list[0] == end_list[0]:
-                for i in range(int(end_list[1]) - int(start_list[1])):
-                    self.coordinates.append(str(start_list[0]) + str(i + int(start_list[1]) + 1))
-            else:
-                i = 0
-                while start_list[0] != letter_list[i]:
-                    i += 1
-                while end_list[0] != letter_list[i + 1]:
-                    self.coordinates.append(str(letter_list[i + 1]) + str(start_list[1]))
-                    i += 1
-                self.coordinates.append(end)
+    def addCoordinates(self, coordintates):  # populate self.coordinates based on start and end positions
+        self.coordinates = coordintates
 
     def addHit(self, coordinate):  # called by the board if a ship is hit; should update bool in hits list
         for i in self.coordinates:
