e48dfbaf08c7ca8c83babf7130441635e4ace30f
EliAlfVGabriel <130845207+EliAlfVGabriel@users.noreply.github.com>
2024-09-22 16:51:17 -0500

    enemy.py

---
+++ enemy.py
@@ -0,0 +1,23 @@
+import random
+
+from board import Board
+from ship import Ship
+
+class Enemy:
+    def __init__(self, ships):
+        self.ships = ships          # number of enemy ships
+
+    def easy(self):
+        pass
+    
+    def medium(self):
+        pass
+
+    def hard(self):
+        pass
+
+    def shoot(self):
+        pass
+
+    def placeShips(self):
+        pass
