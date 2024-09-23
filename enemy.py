3bb19cf0e33b6edb742a3d28c56fd0d099d37eb7
allie1587 <130801516+allie1587@users.noreply.github.com>
2024-09-23 11:16:50 -0500

    Update enemy.py

--- enemy.py
+++ enemy.py
@@ -1,31 +1,41 @@
 import random
-
 from board import Board
-from ship import Ship
 
 class Enemy:
     def __init__(self,numofships):
-        self.numofships = numofships            #number of ships
+        self.numofships = numofships
         self.AIboard = Board()
+        self.theSpecial = 1
 
     def easy(self):
-        while True:
-            letter = random.choice('ABCDEFGHIJ')
-            number = random.randint(1,10)
-            coordinates = f"{letter}{number}"
-            isValid = self.shoot(coordinates)
-            if isValid == True:
-                break
-    
+        special = random.randint(1,10)
+        if special != 5:
+            while True:
+                letter = random.choice('ABCDEFGHIJ')
+                number = random.randint(1,10)
+                coordinates = f"{letter}{number}"
+                isValid = self.shoot(coordinates)
+                if isValid == True:
+                    break
+        else:
+            if self.theSpecial > 0:
+                while True:
+                    letter = random.choice('ABCDEFGHIJ')
+                    number = random.randint(1,10)
+                    coordinates = f"{letter}{number}"
+                    isValid = self.shoot(coordinates)
+                    if isValid == True:
+                        break
+            #finish the special shot
     def medium(self):
         pass
 
-    # hard difficulty
     def hard(self):
         pass
 
     def shoot(self,coordinates):
         return self.AIboard.fireShotAI(coordinates)
-
     def placeShips(self):
+        pass
+    def gameOver(self):
         pass
