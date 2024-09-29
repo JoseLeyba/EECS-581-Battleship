03cbe2ed6383938809a8d2d42fc2318af699b6ba
allie1587 <130801516+allie1587@users.noreply.github.com>
2024-09-29 09:57:18 -0500

    Update driver.py

--- driver.py
+++ driver.py
@@ -1,3 +1,10 @@
+'''
+Authors: Alexandra, Sophia, Eli, Jose, and Riley
+Other Authors: Group 4
+Date: 09/09/2024
+Last modified: 09/15/2024
+Purpose: Starts the game
+'''
 from battleship import Battleship
 
 if __name__ == "__main__":
